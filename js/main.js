664487d848607fc15ae07c5cdecacd31113b32e5
SergMih75 <109480381+SergMih75@users.noreply.github.com>
2023-09-11 23:47:28 +0300

    mobile menu add

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-"use strict"
+'use strict'
+
+const burger = document.querySelector('.burger')
+
+document.addEventListener('click', e => {
+	if (e.target.classList.contains('menu-mobile__link')) {
+		burger.checked = false
+	}
+})
